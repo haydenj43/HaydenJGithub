0705a3d2821db8cde2a145abbe2894f253f4bb3b
haydenj43 <10857468@my.uvu.edu>
2021-11-12 13:04:18 -0700

    buttons getting better

--- Characters/Characters.js
+++ Characters/Characters.js
@@ -1,33 +1,61 @@
 import { people } from '../Characters/people.js'
 
 const mainContent = document.querySelector('#main')
+
+const maleCharacters = people.filter(person => person.gender === 'male')
+const femaleCharacters = people.filter(person => person.gender === 'female')
+const otherCharacters = people.filter((person) => {
+    if (person.gender === 'hermaphrodite' || person.gender === 'n/a' || person.gender === 'none') {
+        return person
+    }
+
+})
+
 const mainBody = document.querySelector('body')
 const header = document.createElement('header')
 const maleButton = document.createElement('button')
 const femaleButton = document.createElement('button')
-header.appendChild(maleButton)
-header.appendChild(femaleButton)
+
+populateDOM(otherCharacters)
+
 mainBody.appendChild(header)
 maleButton.textContent = 'Male Characters'
+maleButton.addEventListener('click', () => populateDOM(maleCharacters))
 femaleButton.textContent = 'Female Characters'
-
+femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
 document.body.insertBefore(header, mainContent)
+header.appendChild(maleButton)
+header.appendChild(femaleButton)
 
-const maleCharacters = people.filter(person => person.gender === 'male')
-const femaleCharacters = people.filter(person => person.gender === 'female')
-console.log(maleCharacters)
 
-people.forEach((element, index) => {
 
-    const charFigure = document.createElement('figure')
-    const charImg = document.createElement('img')
+function populateDOM(characters) {
+    while (mainContent.firstChild) {
+        mainContent.removeChild(mainContent.firstChild)
+    }
+    characters.forEach((element) => {
 
-    charImg.src = `https://starwars-visualguide.com/assets/img/characters/${index + 1}.jpg`
-    const charCaption = document.createElement('figcaption')
-    charCaption.textContent = element.name
+        const charFigure = document.createElement('figure')
+        const charImg = document.createElement('img')
+        const charNum = getLastNumber(element.url)
+        charImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+        const charCaption = document.createElement('figcaption')
+        charCaption.textContent = element.name
 
-    charFigure.appendChild(charImg)
-    mainContent.appendChild(charFigure)
-    charFigure.appendChild(charCaption)
+        charFigure.appendChild(charImg)
+        mainContent.appendChild(charFigure)
+        charFigure.appendChild(charCaption)
 
-})
+    })
+}
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/')
+    let start = end - 2
+
+    if (url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start, end)
+
+}
