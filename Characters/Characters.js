b39209480c9aa6b821c5f9d64bc617c1cb09bd9c
haydenj43 <10857468@my.uvu.edu>
2021-11-14 18:52:10 -0700

    star wars progress

--- Characters/Characters.js
+++ Characters/Characters.js
@@ -4,7 +4,7 @@ const mainContent = document.querySelector('#main')
 
 const maleCharacters = people.filter(person => person.gender === 'male')
 const femaleCharacters = people.filter(person => person.gender === 'female')
-const otherCharacters = people.filter((person) => {
+const fatSlugAndRobots = people.filter((person) => {
     if (person.gender === 'hermaphrodite' || person.gender === 'n/a' || person.gender === 'none') {
         return person
     }
@@ -15,10 +15,13 @@ const mainBody = document.querySelector('body')
 const header = document.createElement('header')
 const maleButton = document.createElement('button')
 const femaleButton = document.createElement('button')
+const otherButton = document.createElement('button')
+populateDOM(people)
 
-populateDOM(otherCharacters)
 
 mainBody.appendChild(header)
+otherButton.textContent = 'Fat Slug and Robots'
+otherButton.addEventListener('click', () => populateDOM(fatSlugAndRobots))
 maleButton.textContent = 'Male Characters'
 maleButton.addEventListener('click', () => populateDOM(maleCharacters))
 femaleButton.textContent = 'Female Characters'
@@ -26,6 +29,7 @@ femaleButton.addEventListener('click', () => populateDOM(femaleCharacters))
 document.body.insertBefore(header, mainContent)
 header.appendChild(maleButton)
 header.appendChild(femaleButton)
+header.appendChild(otherButton)
 
 
 
