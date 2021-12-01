04c3dbcfd9bf4a7b5cdea96045848c132048272f
haydenj43 <10857468@my.uvu.edu>
2021-12-01 16:58:34 -0700

    pokemon progress 1

--- Pokemon Cards/Pokemoncode.js
+++ Pokemon Cards/Pokemoncode.js
@@ -1,12 +1,37 @@
 async function getAPIData(url) {
     try {
         const response = await fetch(url)
-console.log(response)
+const data = await response.json()
+return data
 } catch (error) {
 console.error(error)
     }
     
 }
 
-getAPIData(`https://pokeapi.co/api/v2/pokemon/ditto
-`)
+getAPIData(`https://pokeapi.co/api/v2/pokemon/4
+`).then((data) => {
+    console.log(data)
+    populateCards(data)
+})
+
+const pokeGrid = document.querySelector('.pokeGrid')
+
+
+function populateCards(pokemon) {
+    const pokeFlip = document.createElement('div')
+    pokeFlip.className = 'flip'
+    const pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+    const pokeFront = document.createElement('div')
+    pokeFront.className = 'cardFace front'
+    pokeFront.textContent = 'front'
+    const pokeBack = document.createElement('div')
+    pokeBack.className = 'cardFace back'
+    pokeBack.textContent = 'back'
+
+    pokeCard.appendChild(pokeFront)
+    pokeCard.appendChild(pokeBack)
+    pokeFlip.appendChild(pokeCard)
+    pokeGrid.appendChild(pokeFlip)
+}
